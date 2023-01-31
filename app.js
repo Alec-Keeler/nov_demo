70ee2259551483fc6fbcc200ddda23adfac7f454
Alec Keeler <alec.keeler@gmail.com>
2023-01-30 18:06:35 -0600

    final demo

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ app.get('/test', (req, res) => {
     res.send('Your app is live') // res.json
 })
 
-let data = [];
+let data = [
+    {name: 'Carcassonne'},
+    {name: 'Settlers of Catan'},
+];
 
 app.post('/boardgames', (request, response) => {
     data.push(request.body)
@@ -23,6 +26,38 @@ app.get('/boardgames', (req, res) => {
 
 //put, patch, delete
 //send, json, redirect, render
+
+// Taking in user input
+// req.body, req.query, req.params
+
+app.get('/boardgames/search', (req, res) => {
+    console.log(req.query)
+    if (req.query.name) {
+        for (let i = 0; i < data.length; i++) {
+            const game = data[i];
+            if (game.name === req.query.name) {
+                res.json(data[i])
+            }
+        }
+    } else {
+        res.json(data)
+    }
+})
+
+// /boardgames/banana
+// /boardgames/1
+// /boardgames/search
+app.get('/boardgames/:id', (req, res) => {
+    console.log(req.params)
+
+    if (data[req.params.id]) {
+        res.json(data[req.params.id])
+    } else {
+        res.send('This record does not exist D:')
+    }
+})
+
+
 
 const port = 8000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
