46aac6c608bbf23da42c21e228c07cc09e8acbf2
Alec Keeler <alec.keeler@gmail.com>
2023-02-02 10:54:35 -0600

    related tables

--- sql_practice/bg_data.sql
+++ sql_practice/bg_data.sql
@@ -1,16 +1,41 @@
-INSERT INTO boardgames (name, max_players, genre, avg_rating)
-VALUES
-('Gloomhaven', 4, 'Adventure', 8.8),
-('Pandemic Legacy: Season 1', 4, 'Cooperative', 8.62),
-('Brass: Birmingham', 4, 'Economic', 8.66),
-('Terraforming Mars', 5, 'Economic', 8.43),
-('Twilight Imperium: Fourth Edition', 6, 'Strategy', 8.7),
-('Spirit Island', 4, 'Cooperative', 8.34),
-('Mage Knight', 4, 'Adventure', 8.1),
-('Rising Sun', 5, 'Strategy', 7.88),
-('Mystic Vale', 4, 'Strategy', 8.5),
-('Wingspan', 5, 'Economic', 8.85);
+-- INSERT INTO boardgames (name, max_players, genre, avg_rating)
+-- VALUES
+-- ('Gloomhaven', 4, 'Adventure', 8.8),
+-- ('Pandemic Legacy: Season 1', 4, 'Cooperative', 8.62),
+-- ('Brass: Birmingham', 4, 'Economic', 8.66),
+-- ('Terraforming Mars', 5, 'Economic', 8.43),
+-- ('Twilight Imperium: Fourth Edition', 6, 'Strategy', 8.7),
+-- ('Spirit Island', 4, 'Cooperative', 8.34),
+-- ('Mage Knight', 4, 'Adventure', 8.1),
+-- ('Rising Sun', 5, 'Strategy', 7.88),
+-- ('Mystic Vale', 4, 'Strategy', 8.5),
+-- ('Wingspan', 5, 'Economic', 8.85);
 
-INSERT INTO boardgames (name, avg_rating, genre)
+INSERT INTO boardgames (name, max_players)
 VALUES
-('Terra Mystica', 7.6, 'Strategy');
+('Gloomhaven', 4),
+('Pandemic Legacy: Season 1', 4),
+('Brass: Birmingham', 4),
+('Terraforming Mars', 5),
+('Twilight Imperium: Fourth Edition', 6),
+('Spirit Island', 4),
+('Mage Knight', 4),
+('Rising Sun', 5),
+('Mystic Vale', 4),
+('Wingspan', 5);
+
+-- INSERT INTO boardgames (name, avg_rating, genre)
+-- VALUES
+-- ('Terra Mystica', 7.6, 'Strategy');
+
+INSERT INTO reviews (reviewer_name, comment, rating, boardgame_id)
+VALUES
+('Alec', 'This game is sweet', 10, 6);
+
+INSERT INTO genres (genre)
+VALUES
+('Strategy');
+
+INSERT INTO genre_boardgames (game_id, genre_id)
+VALUES
+(5, 1);
